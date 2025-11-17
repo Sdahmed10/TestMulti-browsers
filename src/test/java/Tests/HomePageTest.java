6eb6c19b4ca738227fab37cf788c163ce68f67e5
Sdahmed10 <s.ahmed198894@gmail.com>
2025-11-17 10:14:06 +0100

    edit home page tests and testng.xml

--- src/test/java/Tests/HomePageTest.java
+++ src/test/java/Tests/HomePageTest.java
@@ -28,29 +28,19 @@ public class HomePageTest extends TestMultiBrowsers {
         // Crée un nouvel objet Home page et assigne la page Playwright
         homePage = new HomePage();
         loginPage = new LoginPage();
+
         loginPage.page = this.page;
         homePage.page = this.page;
 
         // Crée un test nommé dans le rapport
-        test = extent.createTest("Login Test - Verify login functionality");
+        test = extent.createTest("Login Test - Verify login functionality + Home Page");
     }
 
-    @Test(priority = 1)
-    public void testLogin() {
-        try {
-            // Exécution du scénario de login
-            loginPage.loginTest();
-            test.pass("Login executed successfully");  // Succès
-        } catch (Exception e) {
-            // En cas d’erreur, on logue dans le rapport
-            test.fail("Login failed: " + e.getMessage());
-            throw e; // Relance l’exception pour que TestNG marque le test comme échoué
-        }
-    }
     @Test(priority = 2)
     public void testHomepage() {
         try {
             // Exécution du scénario de verification
+            loginPage.loginTest();
             homePage.HomePageTest();
             test.pass("Homepage executed successfully");
         } catch (Exception e) {
